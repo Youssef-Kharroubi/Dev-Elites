05aa391d55c0bee07eca25214c5984571583756a
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-03-06 15:16:49 +0100

    style: enhance button layout and hover effects in header

--- Doxaria-UI/src/components/layout/Header.tsx
+++ Doxaria-UI/src/components/layout/Header.tsx
@@ -2,11 +2,11 @@ export default function Header() {
     return (
         <header className="flex justify-between items-center  px-9 py-4  border-b border-b-light/20">
             <h3 className="text-xl font-bold">Doxaria</h3>
-            <nav className="flex gap-6 text-gray-500">
-                <a href="/" className="hover:text-light">Home</a>
-                <a href="/" className="hover:text-light">Dashboard</a>
-                <a href="/" className="hover:text-light">Model</a>
-                <a href="/" className="hover:text-light">Contact Us</a>
+            <nav className="flex gap-6 ">
+                <a href="/" className=" text-content  p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Home</a>
+                <a href="/" className=" text-content p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Dashboard</a>
+                <a href="/" className=" text-content p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Model</a>
+                <a href="/" className=" text-content p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Contact Us</a>
             </nav>
         </header>
     )
