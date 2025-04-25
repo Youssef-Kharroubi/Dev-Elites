d8910de3d985bac21780ff58891f5c0d62d73502
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-04-25 18:28:14 +0100

    add: connecting with azure ML studio to load models

--- backend/Serving-Backend/src/api/app.py
+++ backend/Serving-Backend/src/api/app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from .routes import init_routes
 
-app = Flask(__name__)
-init_routes(app)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+def create_app(model_path):
+    app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+    init_routes(app, model_path)
+    return app
--- backend/Serving-Backend/src/main.py
+++ backend/Serving-Backend/src/main.py
@@ -1,4 +1,23 @@
-from .api.app import app
+import os
+from azureml.core import Workspace
+from .services.model_loader import download_model
+from .api.app import create_app
 
 if __name__ == "__main__":
-    app.run()
+    try:
+        ws = Workspace.from_config()
+        model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "models", "CNN_model.h5"))
+
+        if os.path.exists(model_path):
+            print(f"Model already exists at: {model_path}")
+        else:
+
+            model_path = download_model(ws)
+            print(f"Model downloaded and ready at: {model_path}")
+
+        app = create_app(model_path)
+
+        app.run(debug=True, host="0.0.0.0", port=5000)
+    except Exception as e:
+        print(f"Failed to start app: {str(e)}")
+        raise
