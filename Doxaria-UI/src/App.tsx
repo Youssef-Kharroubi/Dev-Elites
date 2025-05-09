d3fb80e7e56b6d9790c576f30200d57e3342c6ef
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-09 11:33:10 +0100

    style: new layout for medical care form page

--- Doxaria-UI/src/App.tsx
+++ Doxaria-UI/src/App.tsx
@@ -4,6 +4,8 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Model from "./pages/Model.tsx";
 import AboutUs from "./components/sections/AboutUs.tsx";
+import MedicalModelForm from "./components/sections/MedicalModelForm.tsx";
+import PrescriptionModelForm from "./components/sections/PrescriptionModelForm.tsx";
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
                   <Route path="/" element={<Home />}/>
                   <Route path="/Model" element={<Model/>}/>
                   <Route path="/AboutUs" element={<AboutUs/>}/>
+                  <Route path="/Medical-Form" element={<MedicalModelForm/>}/>
+                  <Route path="/Prescription" element={<PrescriptionModelForm/>}/>
               </Routes>
           </div>
       </Router>
--- Doxaria-UI/src/components/sections/MedicalModelForm.tsx
+++ Doxaria-UI/src/components/sections/MedicalModelForm.tsx
@@ -83,63 +83,72 @@ export default function MedicalModelForm (){
     return (
         <section className="container">
             <div className="flex justify-center items-center my-5">
-                <h1 className="text-4xl text-light ">Here you can digitalize your Medical Care Form  docs!</h1>
+                <h1 className="text-4xl text-light ">Here you can digitalize your <span className="font-bold">Medical Care Form </span> docs!</h1>
             </div>
-            <div>
-                <h3 className=" flex justify-center items-center text-2xl my-5">Just upload your docs here!</h3>
-                {error && (
-                    <div className="text-red-500 text-center mb-4">{error}</div>
-                )}
-                {isUploading && (
-                    <div className="text-white text-center mb-4">Uploading...</div>
-                )}
-                <h3 className="m-2">Number Of Selected Files : {numberOfFiles} </h3>
-                <div className="flex items-center justify-center w-full">
-                    <label htmlFor="dropzone-file"
-                           className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                            <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2"
-                                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                            </svg>
-                            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
-                                className="font-semibold">Click to upload</span> or drag and drop</p>
-                            <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
-                                800x400px)</p>
+            <div className="grid grid-cols-2 gap-4">
+                <div>
+                    <h3 className=" flex justify-center items-center text-2xl my-5 ">Just upload your docs here!</h3>
+                    {error && (
+                        <div className="text-red-500 text-center mb-4">{error}</div>
+                    )}
+                    {isUploading && (
+                        <div className="text-white text-center mb-4">Uploading...</div>
+                    )}
+                    <div className="flex justify-between">
+                        <h3 className="m-2 place-content-center">Number Of Selected Files : {numberOfFiles} </h3>
+                        <div className="flex justify-end align-end">
+                            <button type="button" name="submit" id="submit"
+                                    disabled={!enableSubmit}
+                                    className={`my-2 bg-light/10 border-0 ${
+                                        enableSubmit ? 'hover:bg-light/30' : 'opacity-50 cursor-not-allowed'
+                                    }`}>Submit !
+                            </button>
+
                         </div>
-                        <input id="dropzone-file" type="file" onChange={onFileChange}
-                               className="hidden" accept="image/png,image/jpeg,image/gif"
-                               disabled={isUploading} multiple/>
-                    </label>
-                </div>
+                    </div>
 
-                <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">We’ll
-                    never share your details. Read our
-                    <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                        Privacy Policy
-                    </a>.
-                </p>
-                <div className="flex justify-end align-end">
-                    <button type="button" name="submit" id="submit"
-                            disabled={!enableSubmit}
-                            className={`my-2 bg-light/10 border-0 ${
-                                enableSubmit ? 'hover:bg-light/30' : 'opacity-50 cursor-not-allowed'
-                            }`}>Submit !
-                    </button>
+
+                    <div className="flex items-center justify-center w-full">
+                        <label htmlFor="dropzone-file"
+                               className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2"
+                                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                                </svg>
+                                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                    className="font-semibold">Click to upload</span> or drag and drop</p>
+                                <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX.
+                                    800x400px)</p>
+                            </div>
+                            <input id="dropzone-file" type="file" onChange={onFileChange}
+                                   className="hidden" accept="image/png,image/jpeg,image/gif"
+                                   disabled={isUploading} multiple/>
+                        </label>
+                    </div>
+
+                    <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">We’ll
+                        never share your details. Read our
+                        <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">
+                            Privacy Policy
+                        </a>.
+                    </p>
+
 
                 </div>
+                <MedicalFormDataViewer/>
             </div>
             <div>
                 {fileType && insuranceCompany.length > 0 && (
                     <div className="text-green-500 text-center mb-4 font-bold text-2xl">
-                        <span className="text-white">Your Files Contains : </span> {fileType} <br />
+                        <span className="text-white">Your Files Contains : </span> {fileType} <br/>
                         <span className="text-white">Insurance companies: </span> {insuranceCompany.join(', ')}
                     </div>
                 )}
             </div>
-            <MedicalFormDataViewer/>
+
 
         </section>
     );
