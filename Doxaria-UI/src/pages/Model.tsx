2ee824fbf211587400224d41825973f17bf1cd67
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-09 11:32:15 +0100

    style: new layout for the model page

--- Doxaria-UI/src/pages/Model.tsx
+++ Doxaria-UI/src/pages/Model.tsx
@@ -1,38 +1,20 @@
-import PrescriptionModelForm from "../components/sections/PrescriptionModelForm.tsx";
-import MedicalModelForm from "../components/sections/MedicalModelForm.tsx";
-import {useState} from "react";
+
 
 export default function Model(){
 
-    const [active,setActive] = useState<Number>(0);
-    function handlePrescription(){
-        setActive(0)
-    }
-    function handleMedicalForm(){
-        setActive(1)
-    }
-
-    const RenderForm = (active:Number) =>{
-        if(active==0){
-            return(
-                <PrescriptionModelForm/>
-            )
-        }else{
-            return(
-                <MedicalModelForm/>
-            )
-        }
-    }
     return(
         <section className="container">
-            <div className="flex my-4 gap-x-4  ">
-                <button onClick={handlePrescription} className="hover:bg-light/30 border-0" >Prescription</button>
-                <button onClick={handleMedicalForm} className="hover:bg-light/30 border-0" >Medical Care Form</button>
-            </div>
-            <div>
-                {
-                    RenderForm(active)
-                }
+
+
+            <div className="my-4 gap-x-4  py-40">
+                <h2 className="flex justify-center items-center text-2xl font-bold my-4">CHOOSE DOC TYPE ! </h2>
+                <div className="flex justify-center items-center ">
+                    <a href="/Prescription"
+                       className="hover:bg-light/30 border-2 border-gray-600 p-14 rounded-md m-4 text-2xl font-bold ">Prescription</a>
+                    <a href="/Medical-Form"
+                       className="hover:bg-light/30 border-2 border-gray-600 p-14 rounded-md m-4 text-2xl font-bold">Medical
+                        Care </a>
+                </div>
             </div>
 
         </section>
