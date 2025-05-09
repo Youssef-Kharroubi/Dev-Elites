5c7832f9953b02d9dfd5fca2991f36042ff1e43a
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-09 20:54:30 +0100

    add: models for the data extracted

---
+++ Doxaria-UI/src/models/MedicalCareExtractedData.ts
@@ -0,0 +1,10 @@
+export default interface MedicalCareExtractedData {
+    id_form: String,
+    subscriber_name:String,
+    cnam_code:String,
+    registration_number: String,
+    cin_or_passport:String,
+    address: String,
+    patient_name: String,
+    birth_date:String,
+}
---
+++ Doxaria-UI/src/models/PrescriptionExtractedData.ts
@@ -0,0 +1,5 @@
+export default interface PrescriptionExtractedData {
+    name:String,
+    drName:String,
+    medicines:String
+}
