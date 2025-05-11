a862c98c19f6ee84f4fc0e01b98fc729576d5c91
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-11 13:13:46 +0100

    fix: excel path

--- backend/Serving-Backend/src/services/data_extraction_prescription.py
+++ backend/Serving-Backend/src/services/data_extraction_prescription.py
@@ -13,19 +13,19 @@ class Document_Extractor_Prescription:
     def __init__(self):
         base_path = os.path.dirname(__file__)
         cnn_model_path = os.path.join(base_path, '..', 'models', 'text_classification_model.h5')
+        self.excel_path = os.path.join(base_path, '..', 'models', 'cleaned_file.xlsx')
         self.cnn_model = load_model(cnn_model_path)
         self.VisionTROCR = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-handwritten")
         self.TROCR = TrOCRProcessor.from_pretrained("microsoft/trocr-large-handwritten")
 
     def process_prescription(self, image_path):
         reader = easyocr.Reader(['fr'], gpu=False)
-        excel_path = "../models/cleaned_file.xlsx"
         try:
             image = cv2.imread(image_path)
             if image is None:
                 raise ValueError(f"Failed to load image: {image_path}")
             # predict_text(image_path, excel_path, cnn_model,reader)
-            results = predict_text(image_path,excel_path,self.cnn_model,reader)
+            results = predict_text(image_path,self.excel_path,self.cnn_model,reader)
             return results
         except Exception as e:
             print(f"Error processing prescription image : {str(e)}")
