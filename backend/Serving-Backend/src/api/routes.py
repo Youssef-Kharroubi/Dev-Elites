34be932028b8717e7f3074d6ff65db9c68e5fe24
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-11 11:31:26 +0100

    add: prescription extractor endpoint

--- backend/Serving-Backend/src/api/routes.py
+++ backend/Serving-Backend/src/api/routes.py
@@ -1,11 +1,13 @@
 from flask import request, jsonify
 from ..services.document_processor import DocumentProcessor
 from ..services.data_extraction_medical_care import Document_Extractor_Medical_care
+from ..services.data_extraction_prescription import Document_Extractor_Prescription
 import os
 
 def init_routes(app):
     processor = DocumentProcessor()
     extractor_medical_care = Document_Extractor_Medical_care()
+    extractor_prescription = Document_Extractor_Prescription()
 
     @app.route('/process_document', methods=['POST'])
     def process_document():
@@ -25,15 +27,29 @@ def init_routes(app):
     def health_check():
         return jsonify({"status": "healthy"}), 200
 
-    @app.route("/extracted-data", methods=['POST'])
+    @app.route("/extracted-medical-care-data", methods=['POST'])
     def extract_data():
         if 'image' not in request.files:
-            return jsonify({"error": "No image prvided"}), 400
+            return jsonify({"error": "No image provided"}), 400
         file = request.files['image']
         temp_path = "temp_image.jpg"
         try:
             file.save(temp_path)
             result = extractor_medical_care.process_image(temp_path)
+            return jsonify(result)
+        finally:
+            if os.path.exists(temp_path):
+                os.remove(temp_path)
+
+    @app.route("/extracted-prescription-Data", methods=['POST'])
+    def extract_prescription_data():
+        if 'image' not in request.files:
+            return jsonify({"error": " No image provided"}), 400
+        file = request.files['image']
+        temp_path = "temp_image.jpg"
+        try:
+            file.save(temp_path)
+            result = extractor_prescription.process_prescription(temp_path)
             return jsonify(result)
         finally:
             if os.path.exists(temp_path):
