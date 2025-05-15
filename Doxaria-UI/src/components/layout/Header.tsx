6376f788027eb9ac31de6cd891bc800dcd54c99c
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-15 18:25:48 +0100

    style: implementing the new colors

--- Doxaria-UI/src/components/layout/Header.tsx
+++ Doxaria-UI/src/components/layout/Header.tsx
@@ -1,12 +1,12 @@
 export default function Header() {
     return (
-        <header className="flex justify-between items-center container  border-b border-b-light/20">
-            <h3 className="text-xl font-bold"><a href="/">Doxaria</a></h3>
-            <nav className="flex gap-6 py-2">
-                <a href="/" className=" text-content  p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Home</a>
-                <a href="/" className=" text-content p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Dashboard</a>
-                <a href="/Model" className=" text-content p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">Model</a>
-                <a href="/AboutUs" className=" text-content p-2 rounded-md hover:bg-light/20 hover:text-light transition-colors ease-in-out duration-200  ">About Us</a>
+        <header className="flex justify-between items-center container  border-b border-b-primary/20">
+            <a href="/" className=" "><img src="/images/doxaria.png" alt="heroimage"  className="lg:col-span-3 h-16"></img></a>
+            <nav className="flex gap-6 ">
+                <a href="/" className=" text-secondary  p-2 rounded-md hover:bg-cyan hover:text-light transition-colors ease-in-out duration-200  ">Home</a>
+                <a href="/" className=" text-secondary p-2 rounded-md hover:bg-cyan hover:text-light transition-colors ease-in-out duration-200  ">Dashboard</a>
+                <a href="/Model" className=" text-secondary p-2 rounded-md hover:bg-cyan hover:text-light transition-colors ease-in-out duration-200  ">Model</a>
+                <a href="/AboutUs" className=" text-secondary p-2 rounded-md hover:bg-cyan hover:text-light transition-colors ease-in-out duration-200  ">About Us</a>
             </nav>
         </header>
     )
--- Doxaria-UI/src/components/sections/HeroSection.tsx
+++ Doxaria-UI/src/components/sections/HeroSection.tsx
@@ -4,13 +4,13 @@ export default function HeroSection() {
         <section  >
             <div className="grid grid-cols-1 lg:grid-cols-5 lg:items-center gap-4 lg:gap-16 mt-20 px-9">
                 <div className=" content-center lg:col-span-2  ">
-                    <h3 className="text-2xl lg:text-4xl font-bold text-light text-start leading-tight">
+                    <h3 className="text-2xl lg:text-4xl font-bold text-primary text-start leading-tight">
                         Check Our Latest Model!
                     </h3>
-                    <span className="mt-3 text-md text-gray-400">AI-driven image processing for smarter medical data extraction.</span>
+                    <span className="mt-3 text-md text-secondary">AI-driven image processing for smarter medical data extraction.</span>
                     <div className="mt-4 flex justify-center ">
                         <a href="/Model"
-                           className=" rounded-md border-2 p-2 border-light/80 hover:border-dark hover:bg-light hover:text-dark w-1/3 text-center">
+                           className=" rounded-md border-2 p-2 border-primary hover:border-cyan hover:bg-light hover:text-dark w-1/3 text-center">
                             Click HERE!
                         </a>
                     </div>
