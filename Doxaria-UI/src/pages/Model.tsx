451459d17e71bdd12e36036a8496f88b5c6db04b
Youssef-Kharroubi <youssef.kharroubi@esprit.tn>
2025-05-15 18:23:26 +0100

    style: model selection card colors

--- Doxaria-UI/src/pages/Model.tsx
+++ Doxaria-UI/src/pages/Model.tsx
@@ -10,9 +10,9 @@ export default function Model(){
                 <h2 className="flex justify-center items-center text-2xl font-bold my-4">CHOOSE DOC TYPE ! </h2>
                 <div className="flex justify-center items-center ">
                     <a href="/Prescription"
-                       className="hover:bg-light/30 border-2 border-gray-600 p-14 rounded-md m-4 text-2xl font-bold ">Prescription</a>
+                       className="hover:bg-cyan border-2 border-secondary p-14 rounded-md m-4 text-2xl font-bold hover:text-white ">Prescription</a>
                     <a href="/Medical-Form"
-                       className="hover:bg-light/30 border-2 border-gray-600 p-14 rounded-md m-4 text-2xl font-bold">Medical
+                       className="hover:bg-cyan border-2 border-secondary p-14 rounded-md m-4 text-2xl font-bold hover:text-white">Medical
                         Care </a>
                 </div>
             </div>
